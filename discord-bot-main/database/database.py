4420351379d3e3f16895fa012ba47ed3fbdb1206
six1031 <joejcjam32@gmail.com>
2026-08-09 20:55:18 +0100

    Update database.py

--- discord-bot-main/database/database.py
+++ discord-bot-main/database/database.py
@@ -14,7 +14,7 @@ class Database:
         if self.pool:
             return
 
-        database_url = os.getenv("DATABASE_URL")
+        database_url = os.getenv(postgresql://postgres:rcQEDdaLRUCRVSLAcNsRjOqdrHLdcxvQ@postgres.railway.internal:5432/railway)
 
         if not database_url:
             raise RuntimeError("DATABASE_URL was not found.")
