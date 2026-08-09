d2508033e031580645e7279863e9519a1b0b31a6
six1031 <joejcjam32@gmail.com>
2026-08-09 21:42:54 +0100

    Update database.py

--- database/database.py
+++ database/database.py
@@ -14,7 +14,7 @@ class Database:
         if self.pool:
             return
 
-        database_url = os.getenv(postgresql://postgres:rcQEDdaLRUCRVSLAcNsRjOqdrHLdcxvQ@postgres.railway.internal:5432/railway)
+        database_url = os.getenv("DATABASE_URL")
 
         if not database_url:
             raise RuntimeError("DATABASE_URL was not found.")
